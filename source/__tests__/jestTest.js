843458b8f22278ad828dfa924555590439dbbddd
TristonBabers <Triston.babers.official@gmail.com>
2022-12-06 18:24:07 -0800

    Worked on edit button

--- source/__tests__/jestTest.js
+++ source/__tests__/jestTest.js
@@ -1,7 +1,8 @@
 describe('Basic user flow for Website', () => {
     // First, visit the website
+    //TODO: insert page URL 
     beforeAll(async () => {
-      await page.goto('https://cse110-f2021.github.io/Lab8_Website');
+      await page.goto('');
     });
     // 1. check that local storage is correct: recipes is empty 
     it('1. Check recipes object is empty', async () => {
@@ -12,19 +13,35 @@ describe('Basic user flow for Website', () => {
           expect(recipes).toBe('[]');
     });
 
-    // 2. Click create new recipe: check that page URL is to Editor Page 
+    // 2. Click create new recipe: check that page URL is Editor Page 
     it('2. Check ', async () => {
         //expect().toBe();
       });
 
-    // 3. 
+    // 3. Fill in recipe Form and submit: check local storage is updated with new recipe
     it('3. Check ', async () => {
+        //TODO: fill and submit form 
+        let recipes = await page.evaluate(() => {
+            let recipesLS = localStorage.getItem("recipes");
+            return recipesLS;
+        });
+        expect(recipes).toBe('[]'); // TODO: fill in with expected recipe object
+      });
+
+    // 4. Navigate back to home page: check URL is View Page
+    it('4. Check ', async () => {
         //expect().toBe();
       });
 
-    // 4. 
-    it('3. Check ', async () => {
-        //expect().toBe();
+    // 5. Click to view recipe: check that currRecipe is set correctly
+    it('5. Check ', async () => {
+        let currRecipe = await page.evaluate(() => {
+            let currRecipeLS = localStorage.getItem("currRecipe");
+            return currRecipesLS;
+        });
+        expect(currRecipe).toBe(""); // TODO: fill in with expected currRecipe object
       });
+
+    //...
     
 });
