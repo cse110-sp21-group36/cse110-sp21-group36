9a7dc940bb0638816904cb26cefdde10db580bb1
liquidbunx <dcv004@ucsd.edu>
2022-12-06 16:20:46 -0800

    Update navbar and contact page

--- source/assets/scripts/navbar.js
+++ source/assets/scripts/navbar.js
@@ -1,21 +1,22 @@
-/** 
- * Enable the toggle function in the nav bar for mobile screens.
+/**
+ * Function to see if a class element exists or not. If not, throw error. 
+ * 
+ * @param selector - class object to select
  */
-
-/* Function to see if a class element exists or not. If not, throw error. */
 const getElement = (selector) => {
 	const element = document.querySelector(selector)
-  
 	if (element) return element
 	throw Error(
 	  `Please double check your class names, there is no ${selector} class`
 	)
   }
   
-  /* Function to toggle the nav bar for mobile */
-  const links = getElement('.nav-links')
-  const navBtnDOM = getElement('.nav-btn')
-  
-  navBtnDOM.addEventListener('click', () => {
+/** 
+ * Enable the toggle function in the nav bar for mobile screens when button is 
+ * clicked on. 
+ */
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+navBtnDOM.addEventListener('click', () => {
 	links.classList.toggle('show-links')
-  })
+})
